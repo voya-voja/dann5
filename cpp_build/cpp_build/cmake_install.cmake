62aad3ee70aedf19664d9c6b8fd9ba24d7d19020
nebojsa.vojinovic@rogers.com <nebojsa.vojinovic@rogers.com>
2021-01-17 17:37:49 -0500

    correction of test/cpp/CmakeList.txt and cpp_build/CmakeList.txt pybind11/include reference to its installed python environment clean up of pybind11 sub-folder

--- cpp_build/cpp_build/cmake_install.cmake
+++ cpp_build/cpp_build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Nebojsa.Vojinovic/source/repos/dann5
+# Install script for directory: C:/Users/Nebojsa.Vojinovic/source/repos/dann5/cpp_build
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,23 +32,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Nebojsa.Vojinovic/source/repos/dann5/cpp_build/cpp_build/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Nebojsa.Vojinovic/source/repos/dann5/cpp_build/test/cpp/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Nebojsa.Vojinovic/source/repos/dann5/cpp_build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
